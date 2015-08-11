0d0ec32555499673cab73cb853d1492d698a0f1e
Emil <emiml@japcok-pro>
2015-08-11 12:36:41 +0200

    added empty mainWindow

---
+++ MainWindow.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include <gtkmm.h>
+
+class MainWindow : public Gtk::Window {
+};
+
+#endif /** MAIN_WINDOW_H */
+
--- main.cc
+++ main.cc
@@ -1,8 +1,14 @@
 #include "config.h"
 #include <iostream>
+#include "MainWindow.h"
 
 int main(int argc, char **argv) {
-   gfm_debug("starting gfm...\n");
-   gfm_debug("ending gfm...\n");      
-   return 0;
+    gfm_debug("starting gfm...\n");
+    Gtk::Main app(&argc, &argv);
+
+    MainWindow mainWindow;
+    app.run(mainWindow);
+
+    gfm_debug("ending gfm...\n");      
+    return 0;
 }
