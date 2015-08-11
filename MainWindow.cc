f7893f2761ecb35bc23c92ade23e89cef259d46f
Emil <emiml@japcok-pro>
2015-08-11 12:43:32 +0200

    added configurable window title and application name as constant

--- MainWindow.cc
+++ MainWindow.cc
@@ -1,5 +1,7 @@
 #include "MainWindow.h"
+#include "config.h"
 
 MainWindow::MainWindow() {
     this->set_position(Gtk::WIN_POS_CENTER);
+    this->set_title(APPLICATION_NAME);
 }
--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 #ifndef CONFIG_H
 
+#define APPLICATION_NAME "Gtkmm File Manager"
+#define APPLICATION_VERSION "0.1"
+
 #ifdef DEBUG
 #include <glib.h>
 #  define gfm_debug(args...) g_print (args)
