d225c907a4887160c4ca62e5bec9dc36702c0ad7
Emil <emiml@japcok-pro>
2015-08-12 14:32:17 +0200

    added missing debug files

---
+++ debug.cc
@@ -0,0 +1,18 @@
+#include "debug.h"
+#include <glib.h>
+#include <glibmm/date.h>
+#include <glibmm/datetime.h>
+#include <iostream>
+
+void debug_with_time() {
+    Glib::DateTime right_now = Glib::DateTime::create_now_local();
+    right_now.add_days(2);
+    GDateTime *gdt = right_now.gobj();
+    gchar *date_formated = g_date_time_format(gdt, "%F %R:%S");
+   
+    //the line below is more C++ but gives SIGSEV
+    //Glib::ustring date_formated_s = right_now.format("%F %R:%S");
+    
+    g_print("%s - ", date_formated);
+    g_free(date_formated);
+}
