822f4c3873176a68f411b16ee25c753b9c83b5b0
Emil <emiml@japcok-pro>
2015-08-12 14:26:20 +0200

    added debuging with timestamp

--- config.h
+++ config.h
@@ -5,9 +5,8 @@
 
 #ifdef DEBUG
 #include <glib.h>
-#include <chrono>
-#include <ctime>
-#  define gfm_debug(args...) g_print("%s:", std::asctime(std::chrono::system_clock::now()); g_print (args)
+#include "debug.h"
+#  define gfm_debug(args...) debug_with_time(); g_print (args)
 #else
 #  define gfm_debug(args...) ;
 #endif
