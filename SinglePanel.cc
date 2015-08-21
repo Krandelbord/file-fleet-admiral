0fce80283cc4e32ad1228573b23a97045003e0c4
Emil <emiml@japcok-pro>
2015-08-21 06:26:33 +0200

    added empty treeView and bottom panel

--- SinglePanel.cc
+++ SinglePanel.cc
@@ -1,6 +1,14 @@
 #include "SinglePanel.h"
 
-SinglePanel::SinglePanel() : Gtk::Label("napis") {
-    //does not work in constructor
-    //this->set_text("Example text");
+SinglePanel::SinglePanel() {
+    Gtk::TreeView *filesTreeView = createFilesTreeView();
+    this->add(*filesTreeView);
+
+    Gtk::Label *bottomStatusBar = new Gtk::Label("/home/emil");
+
+    this->pack_end(*bottomStatusBar, Gtk::PackOptions::PACK_SHRINK);
+}
+
+Gtk::TreeView* SinglePanel::createFilesTreeView() {
+    return Gtk::manage(new Gtk::TreeView());
 }
