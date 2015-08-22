558bba9a83ffe513d45f6f50c2b9709411164ef1
Emilian Nowak <Krandelbord@users.noreply.github.com>
2015-08-22 14:22:36 +0200

    added column name and size to treeview

--- SinglePanel.h
+++ SinglePanel.h
@@ -10,7 +10,8 @@ class SinglePanel : public Gtk::VBox {
         SinglePanel(const Glib::ustring& startDirPath);
     private:
         Gtk::TreeView* createFilesTreeView();
-        
+        Glib::RefPtr<Gtk::ListStore> createFakeData();
+        void appendOneFile(Glib::RefPtr<Gtk::ListStore> refListStore, int size, const Glib::ustring& fileName);
 };
 
 #endif /** SINGLE_PANEL_H */
