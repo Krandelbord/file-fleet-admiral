5092b7769709129d23c829468fff2706877f64d8
Emil <emiml@japcok-pro>
2015-08-24 06:36:59 +0200

    connected signal for chown-clicked

--- ToolBar.cc
+++ ToolBar.cc
@@ -1,6 +1,13 @@
 #include "ToolBar.h"
+#include "config.h"
 
 ToolBar::ToolBar() {   
     Gtk::ToolButton* toolItem = new Gtk::ToolButton("chown");
+    toolItem->signal_clicked().connect(sigc::mem_fun(this, &ToolBar::onChownClicked));
     this->append(*toolItem);
+}
+
+
+void ToolBar::onChownClicked() {
+    gfm_debug("chwon clicked\n");
 }
