6223d94d64badefc2f3a26e3e6a7c1fd54fc364a
Emilian Nowak <Krandelbord@users.noreply.github.com>
2015-08-25 18:21:50 +0200

    added printing time with microseconds

--- debug.cc
+++ debug.cc
@@ -8,11 +8,11 @@ void debug_with_time() {
     Glib::DateTime right_now = Glib::DateTime::create_now_local();
     right_now.add_days(2);
     GDateTime *gdt = right_now.gobj();
-    gchar *date_formated = g_date_time_format(gdt, "%F %R:%S");
+    gchar *date_formated = g_date_time_format(gdt, "%F %R:%S%");
    
     //the line below is more C++ but gives SIGSEV
     //Glib::ustring date_formated_s = right_now.format("%F %R:%S");
     
-    g_print("%s - ", date_formated);
+    g_print("%s:%d - ", date_formated, right_now.get_microsecond());
     g_free(date_formated);
 }
