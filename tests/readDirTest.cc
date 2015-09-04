e926f2076c4c042d254672f35ddb9605eacf18e9
Emilian Nowak <Krandelbord@users.noreply.github.com>
2015-09-04 20:40:21 +0200

    added summary for tests

--- tests/readDirTest.cc
+++ tests/readDirTest.cc
@@ -2,7 +2,10 @@
 #include "../PathResolver.h"
 using namespace snowhouse;
 
-void checkDirGeneration(const Glib::ustring& startDir, const Glib::ustring& cdCommand, const Glib::ustring expected) {
+/**
+ * Returns true for cussess 
+ */
+bool checkDirGeneration(const Glib::ustring& startDir, const Glib::ustring& cdCommand, const Glib::ustring expected) {
     PathResolver path(startDir);
     path.changeDirBy(cdCommand);
 
@@ -10,11 +13,35 @@ void checkDirGeneration(const Glib::ustring& startDir, const Glib::ustring& cdCo
         Assert::That(path.toString(), Equals(expected));
     } catch (AssertionException& ex) {
         std::cout << ex.GetMessage() << std::endl;
+        return false;
     }
+    return true;
 }
 
+class Runner {
+    public :
+        void run(bool runResultWasSuccess) {
+            totalCount++;
+            if (runResultWasSuccess) {
+                successCount++;
+            }
+        }
+
+        void showStats() {
+            std::cout << "Tests finished. Success " << successCount << " of " << totalCount << std::endl;
+        }
+    private :
+        unsigned int totalCount = 0;
+        unsigned int successCount = 0;
+};
+
 int main() {
-    checkDirGeneration("/home/emil/Documents", "notes", "/home/emil/Documents/notes");
-    checkDirGeneration("/home/emil/Documents", "..", "/home/emil");
+    Runner runner;
+
+    runner.run(checkDirGeneration("/home/emil/Documents", "notes", "/home/emil/Documents/notes"));
+    runner.run(checkDirGeneration("/home/emil/Documents", "..", "/home/emil"));
+    runner.run(checkDirGeneration("/", "..", "/"));
+
+    runner.showStats();
 }
 
