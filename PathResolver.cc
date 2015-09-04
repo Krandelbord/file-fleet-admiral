572d5477fdc50ddf012412b7ce1c24e774782cea
Emilian Nowak <Krandelbord@users.noreply.github.com>
2015-09-04 20:24:00 +0200

    fixed changeing directory calculation

--- PathResolver.cc
+++ PathResolver.cc
@@ -1,13 +1,18 @@
 #include "PathResolver.h"
 
 PathResolver::PathResolver(const Glib::ustring& startPath) {
-
+    this->pathAsString = Glib::ustring(startPath);
 }
 
 PathResolver PathResolver::changeDirBy(const Glib::ustring& changeDirPath) {
+    if (changeDirPath == "..") {
+    } else {
+        this->pathAsString.append("/");
+        this->pathAsString.append(changeDirPath);
+    }
     return PathResolver("");
 }
 
 Glib::ustring PathResolver::toString() const {
-    return "cokolwiek";
+    return pathAsString;
 }
