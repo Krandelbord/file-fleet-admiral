b33b6185d1be6f62c3adbbd0df4951b34dfab95e
Emil <emiml@japcok-pro>
2015-09-05 22:54:22 +0200

    added progress indicator

--- PanelHeader.cc
+++ PanelHeader.cc
@@ -4,10 +4,18 @@ PanelHeader::PanelHeader(const Glib::ustring& labelTxt) {
     headerLabel.set_text(labelTxt);
     headerLabel.set_ellipsize(Pango::EllipsizeMode::ELLIPSIZE_MIDDLE);
     this->add(headerLabel);
-    Gtk::Spinner* spinner = Gtk::manage(new Gtk::Spinner());
+    this->spinner = Gtk::manage(new Gtk::Spinner());
     this->add(*spinner);
 }
 
 void PanelHeader::setCurrentDir(const Glib::ustring& newDir) {
     headerLabel.set_text(newDir);
+}
+
+void PanelHeader::startProgress() {
+    this->spinner->start();
+}
+
+void PanelHeader::stopProgress() {
+    this->spinner->stop();
 }
--- PanelHeader.h
+++ PanelHeader.h
@@ -6,9 +6,12 @@ class PanelHeader : public Gtk::HBox {
     public:
         PanelHeader(const Glib::ustring& labelTxt);
         void setCurrentDir(const Glib::ustring& newLabel);
+        void stopProgress();
+        void startProgress();
 
     private:
         Gtk::Label headerLabel;
+        Gtk::Spinner *spinner;
 };
 
 #endif /** PANEL_HEADER_H */
