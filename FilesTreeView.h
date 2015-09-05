1fcdf7a9f026c873299e103a1829a9f9394a9292
Emil <emiml@japcok-pro>
2015-09-05 10:20:50 +0200

    refactor - extracted method for appending column to treeView

--- FilesTreeView.h
+++ FilesTreeView.h
@@ -2,9 +2,14 @@
 #define FILES_TREE_VIEW_H
 
 #include <gtkmm.h>
+
 class FilesTreeView : public Gtk::TreeView {
     public :
         FilesTreeView(Glib::RefPtr<Gtk::ListStore> filesListStorage);
+
+    private:
+    void addStyleByTypeTxtColumn(const Gtk::TreeModelColumn<Glib::ustring> &columnToAdd,
+                                 const Glib::ustring &columnTitle);
 };
 
 #endif /** FILES_TREE_VIEW_H **/
