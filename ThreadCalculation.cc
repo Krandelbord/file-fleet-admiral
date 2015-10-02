540a1fcc5e6ee1a631a7524f86256a12dba1b02a
Emilian Nowak <emil.nowak@gmail.com>
2015-10-02 23:40:16 +0200

    added readl reading of fileSize and type

--- ThreadCalculation.cc
+++ ThreadCalculation.cc
@@ -1,4 +1,5 @@
 #include "ThreadCalculation.h"
+#include <sys/stat.h>
 #include "config.h"
 
 void ThreadCalculation::threadFunction(std::shared_ptr<ThreadMessage> threadMessage) {
@@ -14,10 +15,35 @@ void ThreadCalculation::threadFunction(std::shared_ptr<ThreadMessage> threadMess
 	for (Glib::DirIterator dirIter = dir.begin(); dirIter != dir.end(); ++dirIter) {
         std::string nextElemInDir = *dirIter;
 		std::string path = Glib::build_filename(dirToRead, nextElemInDir);
-		//Glib::usleep(101000);
-        threadMessage->addNewDataAsync(FileListElement(nextElemInDir, 0, FileType::DIRECTORY));
-        readPositionsCount++;
+		Glib::usleep(101000);
+
+        if (nextElemInDir.size()!=0) {
+            __off_t sizeInBytes = readFileSize(path);
+            FileType fileType = readFileType(path);
+            threadMessage->addNewDataAsync(FileListElement(nextElemInDir, sizeInBytes, fileType));
+            readPositionsCount++;
+        }
     }
     gfm_debug("finish of calculation for dir %s, found %d\n", dirToRead.c_str(), readPositionsCount);
 }
 
+__off_t ThreadCalculation::readFileSize(const std::string& filePathToReadSize) {
+    struct stat statFile;
+    __off_t fileSize = 0;
+    int err = stat(filePathToReadSize.c_str(), &statFile);
+    if (err != 0) {
+        fileSize = 0;
+    } else {
+        fileSize = statFile.st_size;
+    }
+    return fileSize;
+}
+
+FileType ThreadCalculation::readFileType(const std::string& pathToReadFileType) {
+    FileType fileType = FileType::REGULAR_FILE;
+    if (Glib::file_test(pathToReadFileType, Glib::FILE_TEST_IS_DIR)) {
+        fileType = FileType::DIRECTORY;
+    }
+
+    return fileType;
+}
