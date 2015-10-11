bf289a5ccaef543fc2133e75e78dfa9953c78628
emil <emil@galeon>
2015-10-11 10:34:19 +0200

    moved convert function to separate file, extended tests

--- SizeDisplayFormat.h
+++ SizeDisplayFormat.h
@@ -12,17 +12,6 @@ enum class SizeDisplayFormat {
     HR_FULL,
 };
 
-std::string convertSizeDFToString(SizeDisplayFormat toConvert) {
-    switch (toConvert) {
-        case SizeDisplayFormat::IN_BYTES:
-            return "IN_BYTES";
-        case SizeDisplayFormat::SPACE_SEPARATED:
-            return "SPACE_SEPARATED";
-        case SizeDisplayFormat::HR_ROUNDED:
-            return "HR_ROUNDED";
-        case SizeDisplayFormat::HR_FULL:
-             return "HR_FULL";
-    }
-    throw std::runtime_error("invalid enum value ");
-}
+std::string convertSizeDFToString(SizeDisplayFormat toConvert);
+
 #endif /** SIZE_DISPLAY_FORMAT_H */
--- SizeFormatter.h
+++ SizeFormatter.h
@@ -1,11 +1,16 @@
 #ifndef SIZE_FORMATTER_H
 #define SIZE_FORMATTER_H
 
+#include <bits/stringfwd.h>
+#include "SizeDisplayFormat.h"
+
 /**
  * Format size from bytes to given format
  */
 class SizeFormatter {
 
+public:
+    static std::string formatSize(unsigned long bytes, SizeDisplayFormat format);
 };
 
 #endif //SIZE_FORMATTER_H
