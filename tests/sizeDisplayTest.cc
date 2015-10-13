1ce270a1cd922701daf7910b5ceb8426cf515b1d
emil <emil@galeon>
2015-10-13 17:13:55 +0200

    added spaces formatter for size

--- tests/sizeDisplayTest.cc
+++ tests/sizeDisplayTest.cc
@@ -4,6 +4,7 @@
 #include "Runner.h"
 #include "../SizeFormatter.h"
 #include "../SizeFormatterSimple.h"
+#include "../SizeFormatterSpaces.h"
 #include <iostream>
 bool checkSizeFormatting(SizeFormatter* formatter, size_t sizeToFormatInBytes, const std::string expectedOut) {
 
@@ -14,7 +15,14 @@ bool checkSizeFormatting(SizeFormatter* formatter, size_t sizeToFormatInBytes, c
 int main() {
     std::vector<std::function<bool()>> testsToRun;
     SizeFormatter *sizeFormatterSimple = new SizeFormatterSimple();
-    testsToRun.push_back(std::bind(checkSizeFormatting, sizeFormatterSimple, 12345, "12 345"));
+    testsToRun.push_back(std::bind(checkSizeFormatting, sizeFormatterSimple, 12345, "12345"));
+
+    SizeFormatter *sizeFormatterSpaces = new SizeFormatterSpaces();
+    testsToRun.push_back(std::bind(checkSizeFormatting, sizeFormatterSpaces, 12345, "12 345"));
+    testsToRun.push_back(std::bind(checkSizeFormatting, sizeFormatterSpaces, 99999, "99 999"));
+    testsToRun.push_back(std::bind(checkSizeFormatting, sizeFormatterSpaces, 99999212, "99 999 212"));
+    testsToRun.push_back(std::bind(checkSizeFormatting, sizeFormatterSpaces, 12, "12"));
+
     Runner runner;
     for (auto oneTestToRun : testsToRun) {
         runner.run(oneTestToRun);
@@ -22,4 +30,5 @@ int main() {
 
     runner.showStats();
     std::cout << "The end";
+    return 0;
 }
