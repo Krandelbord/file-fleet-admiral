1a0354141f07652aa17f7a39afeb79bba352f4f7
Emilian Nowak <emil@dellmill>
2015-10-14 12:58:23 +0200

    fixed displayer with spaces

--- SizeFormatterSpaces.cc
+++ SizeFormatterSpaces.cc
@@ -6,7 +6,7 @@ std::string SizeFormatterSpaces::formatSize(size_t bytes) const {
 
     std::string string = strStream.str();
     size_t placeToPutSpace = string.length() - SPACE_SEPARATOR_EVERY;
-    for (; placeToPutSpace != std::string::npos; placeToPutSpace+=(-SPACE_SEPARATOR_EVERY)) {
+    for (; placeToPutSpace < string.length() && placeToPutSpace > 0; placeToPutSpace+=(-SPACE_SEPARATOR_EVERY)) {
         string.insert(placeToPutSpace, " ");
     }
     return string;
