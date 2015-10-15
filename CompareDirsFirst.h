e84b98fe778b1bfa96c46142f159dbaddca6665d
Emilian Nowak <Emilian Nowak@localhost>
2015-10-15 22:36:36 +0200

    when sorting dirs first, make them sort alphabeticallyy

--- CompareDirsFirst.h
+++ CompareDirsFirst.h
@@ -7,5 +7,7 @@ class CompareDirsFirst : public CompareFunction {
     public:
         virtual CompareResult compare(const FileListElement& first, const FileListElement& second) const;
         virtual ~CompareDirsFirst();
+
+    bool bothAreDirs(const FileListElement &element, const FileListElement &listElement) const;
 };
 #endif /** COMPARE_DIRS_FIRST_H */
